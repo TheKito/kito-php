e70ec1b31f2d815a9b83ddd9a6da2c10c236fad8
TheKito <11139894+TheKito@users.noreply.github.com>
2020-04-12 12:52:10 -0300

    Create makeExceptionsTests.php

---
+++ makeExceptionsTests.php
@@ -0,0 +1,59 @@
+<?php
+
+define('baseDir', __DIR__ . DIRECTORY_SEPARATOR . 'src');
+define('baseVendor', 'Kito');
+
+
+
+$out = '<?php
+use PHPUnit\Framework\TestCase;
+
+class ExceptionTest extends TestCase
+{';
+
+scan(baseDir . DIRECTORY_SEPARATOR);
+
+$out.='}';
+
+file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'tests' .DIRECTORY_SEPARATOR .'ExceptionTest.php', $out);
+function scan($path) {
+    foreach (scandir($path) as $name) {
+        if ($name == '.')
+            continue;
+        if ($name == '..')
+            continue;
+
+        $subPath = $path . $name;
+
+        if (is_file($subPath))
+            scanFile($subPath);
+
+        if (is_dir($subPath))
+            scan($subPath . DIRECTORY_SEPARATOR);
+    }
+}
+
+function scanFile($path) {
+    if (substr($path, -13) != 'Exception.php')
+        return;
+    
+    $path = substr($path, 0,-4);
+    
+    $ns = str_replace('/', '\\', baseVendor . substr($path, strlen(baseDir)));
+    $fn = 'test'.str_replace('\\', '', $ns);
+    
+    global $out;
+$out.= '
+    public function '.$fn.'()
+    {        
+        $this->expectException('.$ns.'::class);
+        throw new '.$ns.'("Test");
+    }
+' ;   
+    
+    
+    
+    
+}
+
+
