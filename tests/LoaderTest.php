0ca91ba9e078ca6713230709e99675d511ac892b
TheKito <11139894+TheKito@users.noreply.github.com>
2020-04-12 10:40:39 -0300

    Update LoaderTest.php

--- tests/LoaderTest.php
+++ tests/LoaderTest.php
@@ -3,6 +3,10 @@ use PHPUnit\Framework\TestCase;
 
 class LoaderTest extends TestCase
 {
+    public function testLoader()
+    {        
+        $this->assertSame(class_exists('Kito\Loader\Loader'), true);
+    }
     public function testLoaderSources()
     {        
         $this->assertSame(class_exists('Kito\Loader\Sources'), true);
