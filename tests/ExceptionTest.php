e4fee47391284f933d3b06295231badea5cde127
TheKito <11139894+TheKito@users.noreply.github.com>
2020-04-12 12:32:22 -0300

    Update ExceptionTest.php

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -6,7 +6,7 @@ class ExceptionTest extends TestCase
     public function testLoaderException()
     {        
         $this->expectException(Kito\Exception::class);
-        throw new Exception('Test');
+        throw new Kito\Exception('Test');
     }
 
 }
