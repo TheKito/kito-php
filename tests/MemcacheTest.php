0e95205da4b834654a1f91a5301996f12f13070c
TheKito <11139894+TheKito@users.noreply.github.com>
2020-04-12 11:13:47 -0300

    Update MemcacheTest.php

--- tests/MemcacheTest.php
+++ tests/MemcacheTest.php
@@ -1,20 +1,19 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
-class MemcacheTest extends TestCase
-{
-    public function testMemcache()
-    {        
+class MemcacheTest extends TestCase {
+
+    public function testMemcache() {
         $this->assertSame(class_exists('Kito\Storage\Memcache'), true);
     }
-    public function testInstanceMemcache($keyPrefix = null)
-    {          
 
-            new Kito\Storage\Memcache($keyPrefix);
-
+    public function testInstanceMemcache($keyPrefix = null) {
+        $this->assertInstanceOf(Kito\Storage\Memcache::class, new Kito\Storage\Memcache($keyPrefix));
     }
-    public function testInstanceMemcacheKeyPrefix()            
-    {
+
+    public function testInstanceMemcacheKeyPrefix() {
         $this->testInstanceMemcache('ExampleKeyPrefix');
     }
+
 }
