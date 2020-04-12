a98849c625012b043f2b948322449f67e4d6c1b3
TheKito <11139894+TheKito@users.noreply.github.com>
2020-04-12 13:16:12 -0300

    Update Exception.php

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -20,7 +20,7 @@ namespace Kito\Exceptions;
  *
  * @author TheKito < blankitoracing@gmail.com >
  */
-class Exception extends \Exception//\Kito\Exception 
+class Exception extends \Kito\Exception 
 {
     
 }
