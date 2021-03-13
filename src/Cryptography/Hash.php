39d906f6df93a4771dae38cd335ea28d7fd49edd
TheKito <11139894+TheKito@users.noreply.github.com>
2021-03-12 23:08:51 -0300

    move2hash

--- src/Cryptography/Hash.php
+++ src/Cryptography/Hash.php
@@ -12,79 +12,12 @@
  * GNU Lesser General Public License for more details.
  */
 
-namespace Kito\Cryptography;
+namespace Kito\Hash;
 
 /**
  * @author TheKito < blankitoracing@gmail.com >
  */
-class Hash
+class Hash extends \Kito\Hash\HashFunction
 {
-    public static function getAlgorithms(): array
-    {
-        return hash_algos();
-    }
-
-    public static function getAlgorithm(string $name): Hash
-    {
-        $lowerName = strtolower($name);
-
-        static $_ = null;
-
-        if ($_ === null) {
-            $_ = [];
-        }
-
-        if (!isset($_[$lowerName])) {
-            $_[$lowerName] = new Hash($lowerName);
-        }
-
-        return $_[$lowerName];
-    }
-
-    private $name;
-    private $example;
-
-    private function __construct(string $name)
-    {
-        $this->name = $name;
-
-        if (!in_array($this->name, self::getAlgorithms())) {
-            throw new HashAlgorithmNotFoundException($this->name);
-        }
-
-        $this->example = $this->calc('');
-    }
-
-    public function calc(string $data): string
-    {
-        $t = hash($this->name, $data);
-
-        if ($t === false) {
-            throw new HashAlgorithmCalcException($data);
-        }
-
-        return strtoupper($t);
-    }
-
-    public function check(string $hashValue, string $data): bool
-    {
-        return $this->calc($data) == strtoupper($hashValue);
-    }
-
-    public function checkHash(string $hashValue): bool
-    {
-        return strlen($hashValue) == strlen($this->example);
-    }
-
-    public function validateHash(string $hashValue): void
-    {
-        if (!$this->checkHash($hashValue)) {
-            throw new InvalidHashValueException($hashValue);
-        }
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
+    
 }
